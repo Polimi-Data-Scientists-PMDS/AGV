50a6bce9a50e7076d5df78fa9b318db3032bd853
matteoroda <matteo.roda@icloud.com>
2026-02-26 12:29:53 +0100

    refactor: Introduced Pioneer 3-DX robot to better fit reality. Renamed files and folders. Updated README.

---
+++ AGV_Webots_World_and_Controllers/controllers/RobotController/RobotController.py
@@ -0,0 +1,232 @@
+"""my_controller controller."""
+
+from controller import Robot  # type: ignore
+#Error due to library used by webots and not imported locally
+from dataclasses import dataclass
+import numpy as np 
+import time
+import math
+
+
+def main():
+    controller = RobotController()
+    controller.set_goal_position((5, 5))
+
+    while controller.isAlive():
+        """
+            1. CHECK IF GOAL IS REACHED
+                IF YES: LOAD NEXT GOAL OR STOP
+        
+            2. UPDATE THE STATE
+
+            3. CALCULATE HEADING ERRORS TO THE GOAL
+                distance error is capped to 2m for better control and obstacle avoidance
+
+            4. CHECK IF THERE ARE OBSTACLES ALONG THE LOCAL PATH
+                (check if the path from the robot to the local goal (2m from it) is free)
+                IF THERE ARE OBSTACLES: ADJUST THE GOAL (heading error)
+
+            5. SEND FINAL ERRORS (position and heading) TO THE CONTROL
+        """
+        # 1. CHECK IF GOAL IS REACHED
+
+        # 2. UPDATE THE STATE
+        controller.state_update()
+
+        # 3. CALCULATE HEADING ERRORS TO THE GOAL
+        distance_error, heading_error = controller.get_control_errors()
+
+        # 4. CHECK IF THERE ARE OBSTACLES ALONG THE LOCAL PATH
+
+        # 5. SEND FINAL ERRORS (position and heading) TO THE CONTROL
+        lin_vel, ang_vel = controller.calculate_velocity(distance_error, heading_error)
+        controller.set_robot_velocity(lin_vel, ang_vel)
+
+
+        # OUTPUT & PRINTING
+        if controller.should_print():
+            # Print distance sensor value
+            print(f"Time: {controller.robot.getTime()}s")
+            # Print lidar values
+            #print(f"Lidar values: L: {left_min}, C: {center_min}, R: {right_min}")
+            # Print GPS values
+            # print(f"GPS values: {gps_values}")
+            # Get wheel speed and print it
+            w_l, w_r = controller.get_wheel_velocity()
+            lin_vel, ang_vel = controller.get_robot_velocity()
+            print(f"\nWheel speeds:\n L: {w_l}, R: {w_r}")
+            print(f"\nRobot speed:\n linear: {lin_vel}, angular: {ang_vel}")
+
+
+@dataclass
+class Position:
+    x: float
+    y: float
+
+@dataclass
+class State:
+    x: float
+    y: float
+    theta: float
+
+    def calculate_errors(self, goal: Position):
+        """ Compute distance and heading errors to a goal position.
+            Returns:
+                rho (float): Euclidean distance to the goal.
+                alpha (float): Heading error to the goal, wrapped to [-pi, pi].
+        """
+        if goal is None:
+            raise Exception("Goal position is None")
+        dx = goal.x - self.x
+        dy = goal.y - self.y
+        rho = np.sqrt(np.square(dx) + np.square(dy))    # distance error
+        alpha = np.arctan2(dy, dx) - self.theta         # heading error
+        alpha = (alpha + np.pi) % (2 * np.pi) - np.pi   # normalized heading error
+        return rho, alpha
+
+
+class RobotController:
+    def __init__(self):
+        # CONSTANTS
+        self.DISTANCE_BETWEEN_WHEELS = 0.052 # (m) distance between wheels found in the robot manual
+        self.WHEEL_RADIUS = 0.02 # (m) wheel radius found in the robot manual
+        self.MAX_WHEEL_SPEED = 12.3 # (rad/s) actual max speed is 100, this setting is to not overspeed the robot
+        self.MAX_VISION_DISTANCE = 2 # (m) distance where the robot can see
+        self.K_DISTANCE = 0.1 # distance gain - TO BE TUNED (probably half of cruise speed)
+        self.K_HEADING = 0.1 # heading gain - TO BE TUNED
+        self.MAX_LIN_VEL = 0.4 # (m/s) - TO BE TUNED
+        self.MAX_ANG_VEL = 1 # (rad/s) - TO BE TUNED
+        self.ANG_VEL_DEADZONE = 0.05 # (rad/s) - TO BE TUNED
+        # self.TURN_SPEED = 0.35 # (m/s)
+        # self.HARD_TURN_SPEED = 0.2 # (m/s)
+
+        # ROBOT
+        self.robot = Robot() # Robot instance
+        self.initial_timestep = int(self.robot.getBasicTimeStep()) # (ms) currently timestep is 1ms
+        
+        self.state = None
+        self.goal_position = None
+
+        # DEVICES
+        # Motors
+        self.motorL = self.robot.getDevice('left wheel motor')
+        self.motorR = self.robot.getDevice('right wheel motor')
+        self.motorL.setPosition(float('inf'))         # setup for velocity control
+        self.motorR.setPosition(float('inf'))         # setup for velocity control
+        print("Motors set up correctly!")
+        # Lidar
+        self.lidar = self.robot.getDevice('Lidar1')
+        self.lidar.enable(self.initial_timestep)
+        print("Lidar set up correctly!")
+        print(f"Lidar FOV: {self.lidar.getFov()} radians ({self.lidar.getFov()*180/np.pi} degrees), horizontal resolution: {self.lidar.getHorizontalResolution()} points, max range: {self.lidar.getMaxRange()}m")
+        # Gps
+        self.gps = self.robot.getDevice('gps')
+        self.gps.enable(self.initial_timestep)
+        print("GPS set up correctly!")
+        print(f"GPS coordinate system: {self.gps.getCoordinateSystem()}")
+
+        # OTHER
+        self.last_print_time = 0
+
+    
+    ##### PERCEPTION #####
+    def state_update(self):
+        # get position and orientation from gps and update the state
+        pass
+
+    def read_lidar(self):
+        raw_range_image = self.lidar.getRangeImage() # List of SP floats what describe the range
+        clean_range_image = [x if x != float('inf') else self.lidar.getMaxRange() for x in raw_range_image] # Remove inf values
+        # Split the list in three sectors (right, center, left) and get minimum of each secor
+        sector_size = self.lidar.getHorizontalResolution() // 3
+        left_sector = np.array(clean_range_image[0 : sector_size])
+        center_sector = np.array(clean_range_image[sector_size : 2*sector_size])
+        right_sector = np.array(clean_range_image[2*sector_size : 3*sector_size])
+        left_min = np.min(left_sector)
+        center_min = np.min(center_sector)
+        right_min = np.min(right_sector)
+        return left_min, center_min, right_min
+    
+
+    ##### CONTROL #####
+    def get_control_errors(self) -> tuple:
+        rho, alpha = self.state.calculate_errors(self.goal_position)
+        rho = min(self.MAX_VISION_DISTANCE, rho) # cap to max vision distance
+        return rho, alpha
+    
+    def calculate_velocity(self, rho, alpha):
+        lin_vel = self.K_DISTANCE * rho * np.cos(alpha)  # cos(alpha) is to slow down forward motion in case alpha is big
+        ang_vel = self.K_HEADING * alpha
+        # linear velocity limits
+        lin_vel = max(0.0, lin_vel)                     # don't go backward
+        lin_vel = min(lin_vel, self.MAX_LIN_VELOCITY)   # max forward speed (tune for your robot)
+        # angular velocity limits  
+        ang_vel = min(self.MAX_ANG_VELOCITY, ang_vel)
+        ang_vel = max(-self.MAX_ANG_VELOCITY, ang_vel)
+        # dead-zone for angular velocity (avoid oscillations near zero)
+        if abs(ang_vel) < self.ANG_VEL_DEADZONE:
+            ang_vel = 0.0
+        return lin_vel, ang_vel
+    
+    def set_robot_velocity(self, lin_vel, ang_vel):
+        # from set linear and angular velocity to wheel linear velocity
+        v_r = lin_vel + 0.5 * ang_vel * self.DISTANCE_BETWEEN_WHEELS
+        v_l = lin_vel - 0.5 * ang_vel * self.DISTANCE_BETWEEN_WHEELS
+        # linear -> angular (rad/s)
+        w_r = v_r / self.WHEEL_RADIUS
+        w_l = v_l / self.WHEEL_RADIUS
+        # saturation to preserve curvature
+        scale = max(abs(w_l), abs(w_r)) / self.MAX_WHEEL_SPEED
+        if scale > 1.0:
+            w_l /= scale
+            w_r /= scale
+        # set motor velocity
+        self.motorL.setVelocity(w_l)
+        self.motorR.setVelocity(w_r)
+        return w_l, w_r
+    
+    def get_wheel_velocity(self):
+        w_r = self.motorR.getVelocity()
+        w_l = self.motorL.getVelocity()
+        return w_l, w_r
+
+    def get_robot_velocity(self):
+        w_l, w_r = self.get_actual_wheel_velocity()
+        v_r = w_r * self.WHEEL_RADIUS
+        v_l = w_l * self.WHEEL_RADIUS
+        lin_vel = (v_r + v_l) / 2
+        ang_vel = (v_r - v_l) / self.DISTANCE_BETWEEN_WHEELS
+        return lin_vel, ang_vel
+
+
+    ##### OTHER FUNCTIONS #####
+    def is_alive(self) -> bool:
+        return self.robot.step(self.initial_timestep) != -1
+    
+    def should_print(self) -> bool:
+        current_time = self.robot.getTime()
+        should_print = current_time - last_print_time >= 0.5
+        if should_print:
+            last_print_time = current_time
+        return should_print
+
+
+
+    # def get_acceleration_speed(self, target_speed, current_speed, delta_time, time_to_target):
+    #     if current_speed <= target_speed:
+    #         delta_vel = (target_speed - current_speed) / delta_time
+    #     elif current_speed > target_speed:
+    #         delta_vel = (target_speed - current_speed) / delta_time
+    #     return current_speed + delta_vel*time_to_target
+
+    # def get_angle_to_target(self):
+    #     # Get GPS values
+    #     gps_values = self.gps.getValues()
+    #     # Calculate the angle to the target point
+    #     angle = np.arctan2(self.goal_position[1] - gps_values[1], self.goal_position[0] - gps_values[0])
+    #     return angle
+    
+
+
+if __name__ == "__main__":
+    main()
